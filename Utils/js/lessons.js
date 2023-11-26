a7e9af0d356333ab32486974f1a0e1c564ec1cf8
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-11-26 20:22:22 +0100

    home page added

---
+++ Utils/js/lessons.js
@@ -0,0 +1,76 @@
+export const lessons = [
+  {
+    numLessons: 5,
+    issue: 'Bootstrap 5.3',
+    day: 60, // inicio 27 de Septiembre
+    msg: 'Bootstrap es un marco de diseño (framework) de código abierto creado por Twitter 🎨',
+    myProgress: '🥚>🐣>🐤>🐓>🕊️>🦃>🦅>🪂>🛩️>✈️>👨‍🚀>🚀>🛸>🦸‍♂️',
+
+    exercise: [
+      {
+        numExercise: 1,
+        description: `./Utils/assets/Ej1.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_01',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_01/index.html',
+      },
+      {
+        numExercise: 2,
+        title: '',
+        description: `./Utils/assets/Ej2.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_02',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_02/index.html',
+      },
+      {
+        numExercise: 3,
+        title: '',
+        description: `./Utils/assets/Ej3.png`,
+        urlCode:
+          'hhttps://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_03',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_02/index.html',
+      },
+      {
+        numExercise: 4,
+        title: '',
+        description: `./Utils/assets/Ej4.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_04',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_02/index.html',
+      },
+      {
+        numExercise: 5,
+        title: '',
+        description: `./Utils/assets/Ej5.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_05',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_02/index.html',
+      },
+      {
+        numExercise: 6,
+        title: '',
+        description: `./Utils/assets/Ej6.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_06',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_02/index.html',
+      },
+      {
+        numExercise: 7,
+        title: '',
+        description: `./Utils/assets/Ej7.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_07',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_02/index.html',
+      },
+      {
+        numExercise: 8,
+        title: '',
+        description: `./Utils/assets/Ej8.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-bootstrap-2611-ta31/tree/main/Ejercicio_08',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-bootstrap-2611-ta31/Ejercicio_02/index.html',
+      },
+
+    ],
+  },
+]
